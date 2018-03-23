3b98389a43dfd8c496f2eac5e3b48ae39e43d84b
Jude Battista <jbattista20@my.whitworth.edu>
2018-03-23 00:27:04 -0700

    Add Roguelike tutorial code

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+	public float levelStartDelay = 2f;
+	public float turnDelay = 0.1f;
+	public static GameManager instance = null;
+	public BoardManager boardScript;
+	public int playerFoodPoints = 100;
+	[HideInInspector] public bool playersTurn = true;
+
+	private Text levelText;
+	private GameObject levelImage;
+	private int level = 1;
+	private List<Enemy> enemies;
+	private bool enemiesMoving;
+	private bool doingSetup;
+	private bool skipLoading = true;
+
+	private void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else if (instance != this)
+		{
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+		enemies = new List<Enemy>();
+		boardScript = GetComponent<BoardManager>();
+		InitGame();
+	}
+
+	//This is called each time a scene is loaded.
+	//This should be deprecated in favor of the following
+	//three methods (OnLevelFinishedLoading, OnEnable, OnDisable)
+	//but the delegate appears to fire too many times.
+	/*
+	void OnLevelWasLoaded(int index)
+	{
+		//Add one to our level number.
+		level++;
+		//Call InitGame to initialize our level.
+		InitGame();
+	}
+	*/
+	
+	//This is called each time a scene is loaded
+	void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode) {
+		if (skipLoading) {
+			skipLoading = false;
+			return;
+		}
+		//Increment level number
+		level++;
+		//Initialize level
+		InitGame();
+	}
+
+	void OnEnable()
+	{
+		//Tell our 'OnLevelFinishedLoading' function to start listening for a scene change
+		//event as soon as this script is enabled;
+		SceneManager.sceneLoaded += OnLevelFinishedLoading;
+	}
+
+	void OnDisable()
+	{
+		//Tell our OnLevelFinishedLoading fcn to stop listening for a scene change event as soon as
+		//this script is disabled. Unsubscribe from every delegate you subscribe too!
+		SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+	}
+
+	void InitGame() {
+		doingSetup = true;
+		levelImage = GameObject.Find("LevelImage");
+		levelText = GameObject.Find("LevelText").GetComponent<Text>();
+		levelText.text = "Day " + level;
+		levelImage.SetActive(true);
+		Invoke("HideLevelImage", levelStartDelay);
+
+		enemies.Clear();
+		boardScript.SetupScene(level);
+	}
+
+	private void HideLevelImage() {
+		levelImage.SetActive(false);
+		doingSetup = false;
+	}
+
+	public void GameOver() {
+		levelText.text = "After " + level + " days, you starved.";
+		levelImage.SetActive(true);
+		enabled = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (playersTurn || enemiesMoving || doingSetup) {
+			return;
+		}
+		StartCoroutine(MoveEnemies());
+	}
+
+	public void AddEnemyToList(Enemy script) {
+		enemies.Add(script);
+	}
+
+	IEnumerator MoveEnemies() {
+		enemiesMoving = true;
+		yield return new WaitForSeconds(turnDelay);
+		if (enemies.Count == 0) {
+			yield return new WaitForSeconds(turnDelay);
+		}
+		for (int ndx = 0; ndx < enemies.Count; ndx++) {
+			enemies[ndx].MoveEnemy();
+			yield return new WaitForSeconds(enemies[ndx].moveTime);
+		}
+		playersTurn = true;
+		enemiesMoving = false;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MovingObject {
+	public int wallDamage = 1;
+	public int pointsPerFood = 10;
+	public int pointsPerSoda = 20;
+	public float restartLevelDelay = 1f;
+	public Text foodText;
+
+	public AudioClip moveSound1;
+	public AudioClip moveSound2;
+	public AudioClip eatSound1;
+	public AudioClip eatSound2;
+	public AudioClip drinkSound1;
+	public AudioClip drinkSound2;
+	public AudioClip gameOverSound;
+
+	private Animator animator;
+	private int food;
+	private Vector2 touchOrigin = -Vector2.one;
+
+
+	// Use this for initialization
+	protected override void Start () {
+		animator = GetComponent<Animator>();
+		food = GameManager.instance.playerFoodPoints;
+		foodText.text = "Food: " + food;
+		base.Start();
+	}
+
+	private void OnDisable()
+	{
+		GameManager.instance.playerFoodPoints = food;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!GameManager.instance.playersTurn) {
+			return;
+		}
+		int horizontal = 0;
+		int vertical = 0;
+#if UNITY_STANDALONE || UNITY_WEBPLAYER || UNITY_EDITOR
+		//Keyboard driven
+		horizontal = (int)Input.GetAxisRaw("Horizontal");
+		vertical = (int)Input.GetAxisRaw("Vertical");
+
+		//No diagonal movement
+		if (horizontal != 0) {
+			vertical = 0;
+		}
+
+#else
+		//Swipe driven
+		if (Input.touchCount > 0) {
+			Touch myTouch = Input.touches[0];
+			if (myTouch.phase == TouchPhase.Began)
+			{
+				touchOrigin = myTouch.position;
+			}
+			else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0) {
+				Vector2 touchEnd = myTouch.position;
+				float x = touchEnd.x - touchOrigin.x;
+				float y = touchEnd.y - touchOrigin.y;
+				touchOrigin.x = -1;
+				if (Mathf.Abs(x) > Mathf.Abs(y))
+				{
+					horizontal = x > 0 ? 1 : -1;
+				}
+				else {
+					vertical = y > 0 ? 1 : -1;
+				}
+
+			}
+		}
+
+#endif
+
+		if (horizontal + vertical != 0) {
+			//Because this is a player moving, it expects to possibly interact with a wall
+			AttemptMove<Wall>(horizontal, vertical); 
+		}
+	}
+
+	protected override void AttemptMove<T>(int xDir, int yDir)
+	{
+		//Movement takes food
+		food--;
+		foodText.text = "Food: " + food;
+		base.AttemptMove<T>(xDir, yDir);
+		RaycastHit2D hit;
+		if (Move(xDir, yDir, out hit)) {
+			SoundManager.instance.RandomizeSfx(moveSound1, moveSound2);
+		}
+		CheckIfGameOver();
+		GameManager.instance.playersTurn = false;
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Exit")
+		{
+			Invoke("Restart", restartLevelDelay);
+			enabled = false;
+		}
+		else if (other.tag == "Food")
+		{
+			food += pointsPerFood;
+			foodText.text = "+" + pointsPerFood + " Food; " + food;
+			SoundManager.instance.RandomizeSfx(eatSound1, eatSound2);
+			other.gameObject.SetActive(false);
+		}
+		else if (other.tag == "Soda") {
+			food += pointsPerSoda;
+			foodText.text = "+" + pointsPerSoda + " Food; " + food;
+			SoundManager.instance.RandomizeSfx(drinkSound1, drinkSound2);
+			other.gameObject.SetActive(false);
+		}
+	}
+
+	protected override void OnCantMove<T>(T component) {
+		Wall hitWall = component as Wall;
+		hitWall.DamageWall(wallDamage);
+		animator.SetTrigger("PlayerChop");
+	}
+
+	private void Restart() {
+		//Application.LoadLevel(Application.loadedLevel);
+		//Deprecated in favor of the following
+		SceneManager.LoadScene(0);
+		
+	}
+
+	public void LoseFood(int loss) {
+		animator.SetTrigger("PlayerHit");
+		food -= loss;
+		foodText.text = "-" + loss + " Food; " + food;
+		CheckIfGameOver();
+	}
+
+	private void CheckIfGameOver() {
+		if (food <= 0) {
+			SoundManager.instance.PlaySingle(gameOverSound);
+			SoundManager.instance.musicSource.Stop();
+			GameManager.instance.GameOver();
+		}
+	}
+}
